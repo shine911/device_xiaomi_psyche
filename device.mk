4e41b45aacae5dfeaaf668ea8ca8229159495dde
Zinadin Zidan <zidan.roking@gmail.com>
2023-09-18 08:59:28 +0700

    psyche: Guard qti thermal 2.0 service hals
    * Its crashes on renoir
    
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    - Also crashes on Alioth
    
    Signed-off-by: Shine <deathbook1999@gmail.com>

--- device.mk
+++ device.mk
@@ -637,8 +637,10 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.sys.thermal.data.path=/data/vendor/thermal/
